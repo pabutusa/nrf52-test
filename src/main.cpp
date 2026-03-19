106720e0aa34d777075f3811a49d0645ec4261f5
Rob Roschewsk <rob@pabut.org>
2026-03-19 14:03:09 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+//#include <RadioLib.h>
+
+// put function declarations here:
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  delay(1000);
+  Serial.print("ping ...");
+
+}
